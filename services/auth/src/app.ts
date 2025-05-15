fb2c2de99b568cba0151f9b1264559553e6eb03b
Maxrosoft <maxfilipenko07@gmail.com>
2025-05-15 23:18:28 +0200

    feat(auth): configure PostgreSQL and Redis integration

--- services/auth/src/app.ts
+++ services/auth/src/app.ts
@@ -1,4 +1,5 @@
 import express, { Express } from "express";
+import sequelize from "./config/sequelize";
 import "dotenv/config";
 
 const PORT = process.env.PORT || 3001;
@@ -17,6 +18,12 @@ app.get("/status", (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Auth service is running on port ${PORT}`);
-});
+(async () => {
+    try {
+        await sequelize.sync();
+        console.log(`Connection with ${process.env.POSTGRES_DB} has been established successfully.`);
+        console.log(`Auth service is running on port ${PORT}`);
+    } catch (error) {
+        console.error(`Unable to connect to ${process.env.POSTGRES_DB}:`, error);
+    }
+})();
